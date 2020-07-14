8decb1d47ce08c3d7054cc7213706c94231dea15
hayatomato-14 <68255185+hayatomato-14@users.noreply.github.com>
2020-07-14 16:56:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,19 @@
       }, 1000);
     });
   
-  $(".nav-item").on('click', function(event) {
+  $('.nav-item').on('click', function(e) {
+    if (this.hash !== '') {
+      e.preventDefault();
 
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+      const hash = this.hash;
 
-        // Store hash
-        var hash = this.hash;
-
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
+      $('html, body').animate(
+        {
           scrollTop: $(hash).offset().top
-        }, 800, function(){
-
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+        },
+        1000
+      );
+    }
+  });
 
 });
