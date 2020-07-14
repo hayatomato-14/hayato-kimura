569c4702a529eaf1bd68a2ab2152b725c3c9f2b9
hayatomato-14 <68255185+hayatomato-14@users.noreply.github.com>
2020-07-14 20:22:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
  $(function(){
 
 
-  $('.navbar-brand').click(function(){
-      $('html, body').animate({
-        scrollTop: 0
-      }, 1000);
-    });
+//  $('.navbar-brand').click(function(){
+//      $('html, body').animate({
+//        scrollTop: 0
+//      }, 1000);
+//    });
   
-  $('.nav-item').on('click', function(e) {
-    if (this.hash !== '') {
-      e.preventDefault();
+//  $('.nav-item').on('click', function(e) {
+//    if (this.hash !== '') {
+//      e.preventDefault();
 
-      const hash = this.hash;
+//      const hash = this.hash;
 
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        1000
-      );
-    }
-  });
+//      $('html, body').animate(
+//        {
+//          scrollTop: $(hash).offset().top
+//        },
+//        1000
+//      );
+//    }
+//  });
 
-});
+// });
